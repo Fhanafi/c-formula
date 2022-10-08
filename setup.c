a4fd3cdfd3db551f11b4027b0671293bd89a9e99
Han <89624707+Fhanafi@users.noreply.github.com>
2022-10-09 06:19:47 +0700

    update setup.c

--- setup.c
+++ setup.c
@@ -4,7 +4,7 @@
 
 void main() {
   int pilih;
-  printf("Angka 1 untuk rumus fisika \nAngka 2 untuk suhu");
+  printf("Angka 1 untuk rumus fisika \nAngka 2 untuk suhu \nAngka 3 untuk kalkulator");
   scanf("%d", &pilih);
   switch(pilih) {
     case 1:
@@ -12,6 +12,9 @@ void main() {
       break;
     case 2:
       return setSuhu();
+      break;
+    case 3:
+      return calculate();
       break;
     default:
       printf("salah memasukan pilihan");
