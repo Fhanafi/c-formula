3c865f10d518d9d28aef66de71ffbc46ac7f99b5
Han <89624707+Fhanafi@users.noreply.github.com>
2022-10-09 20:04:12 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-RED = '\033[0;31m'
-GRN = '\033[0;31m'
-GRNBG = '\033[0;102m'
-
-clear
-
+clear 
+RED='\033[0;31m'
+GRN='\033[0;31m'
+GRNBG='\033[0;102m'
 echo "${RED}=====> installing package <====="
 sleep 3
-pkg install clang
+pkg install linux
 sleep 0.5
 echo "${GRN}=====> install finished <====="
 sleep 3
