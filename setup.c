c8950024884fce5cb82dd6e2d8ec3837a960e22e
Han <89624707+Fhanafi@users.noreply.github.com>
2022-10-09 07:11:49 +0700

    update setup.c

--- setup.c
+++ setup.c
@@ -5,7 +5,7 @@
 
 void main() {
   int pilih;
-  printf("Angka 1 untuk rumus fisika \nAngka 2 untuk suhu \nAngka 3 untuk kalkulator");
+  printf("Angka 1 untuk rumus fisika \nAngka 2 untuk suhu \nAngka 3 untuk kalkulator \nMasukan Pilihan : ");
   scanf("%d", &pilih);
   switch(pilih) {
     case 1:
